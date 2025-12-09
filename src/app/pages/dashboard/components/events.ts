f9a8d52e3739d1d8b34540b78b09fd567e292ac6
Daorsa Hyseni <daorsahyseni@gmail.com>
2025-12-09 19:20:53 +0100

    more fixes

--- src/app/pages/dashboard/components/events.ts
+++ src/app/pages/dashboard/components/events.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
     selector: 'app-events',
     imports: [],
     template: `
-        <div class="card">
+        <div class="">
 <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=Europe%2FAmsterdam&showPrint=0&title=FLOSSK%20Events&src=ZGFvcnNhaHlzZW5pQGdtYWlsLmNvbQ&color=%23039be5" width="800" height="600" frameborder="0" scrolling="no"></iframe>        </div>
     `
 })
